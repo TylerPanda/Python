df6052d08fe842b47f5615bf06cf8a563d606c36
Panda <mingkaityler@gmail.com>
2017-08-12 16:39:37 +0800

    Able to choose yy-mm and click the search button

--- practice.py
+++ practice.py
@@ -13,12 +13,20 @@ browser.get(url)
 # html.encoding = 'utf-8'
 # sp = BeautifulSoup(html.text, 'html.parser')
 
-select_box = browser.find_element_by_name('yy')
-options = [ x for x in select_box.find_elements_by_tag_name('option')]
-values = [value.get_attribute("value") for value in options]
+years = browser.find_element_by_name('yy')
+years_options = [x for x in years.find_elements_by_tag_name('option')]
+years_values = [value.get_attribute('value') for value in years_options]
+months = browser.find_element_by_name('mm')
+months_options = [x for x in months.find_elements_by_tag_name('option')]
+months_values = [value.get_attribute('value') for value in months_options]
 
-for value in values:
+for value in years_values:
     browser.find_element_by_xpath('//select/option[@value=' + str(value) +']').click()
+    for value in months_values:
+        browser.find_element_by_xpath('//select/option[@value=' + str(value) +']').click()
+        browser.find_element_by_link_text('查詢').click()
+
+        # <a href="#" class="button search">查詢</a>
 
 browser.close()
 
