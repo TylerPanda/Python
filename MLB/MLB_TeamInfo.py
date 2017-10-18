5ed04c38f75c7a5e9e87d1a5a31a0a4d9f9e7538
Panda_Ubuntu <mingkaipanda@gmail.com>
2017-10-18 20:40:49 +0800

    Scrapying the MLB Team Data(Ubuntu_Panda)

--- MLB/MLB_TeamInfo.py
+++ MLB/MLB_TeamInfo.py
@@ -3,37 +3,29 @@ from bs4 import BeautifulSoup;
 from datetime import date;
 
 base_url = 'https://www.baseball-reference.com/leagues/MLB/';
-# MLB_first_year = 1903;
-# current_year = date.today().year;
-MLB_first_year = 2016;
+MLB_first_year = 1903;
 current_year = date.today().year;
+# MLB_first_year = 2017;
+# current_year = date.today().year;
 
-i = 0;
+TeamData = [];
 while MLB_first_year <= current_year:
     url = base_url + str(MLB_first_year) + '.shtml';
     MLB_requesets = requests.get(url);
     MLB_soup = BeautifulSoup(MLB_requesets.text, 'html.parser');
+
     Batting =  MLB_soup.find('div', {'id' : 'all_teams_standard_batting'});
     Batting_title = Batting.findAll('h2')[0].text;
     thead = Batting.find('thead').findAll('tr')[0].text.replace('\n',"  ");
     tbody = Batting.find('tbody');
     trs = tbody.findAll('tr');
-    tr_len = len(trs);
-    # print(tr_len);
-    # print(trs);
     for tr in trs:
-        print(tr.text)
-        print(i)
-        i += 1;
-        # ths = tr.find('th');
-        # tds = tr.find('td');
-        # for th in ths:
-        #     print(th);
+        TeamData.append(MLB_first_year);
+        TeamName = tr.findAll('th')[0].text;
+        tds = tr.findAll('td');
+        TeamData.append(TeamName);
+        for i in range(0, len(tds)):
+            TeamData.append(tds[i].text);
+        print(TeamData);
+        TeamData = [];
     MLB_first_year += 1
-    # print(tbody);
-    # print(thead + '\n');
-    # MLB_first_year += 1
-    # Pitching =  MLB_soup.find('div', {'id' : 'all_teams_standard_pitching'}).findAll('h2')[0].text;
-    # MLB_first_year += 1
-    # print(str(current_year) + "\t" + Batting_title);
-    # print(str(current_year) + "\t" + Pitching);
